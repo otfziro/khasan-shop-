81183020ec15a55696bb983e8607fb895f4b8732
otfziro <klimzenit2011@gmail.com>
2026-05-03 00:15:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,6 @@ const DB_NAME = 'khasan_shop';
 
 let db;
 
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: '1h' }));
-
-
 // Connect to MongoDB
 async function connectDB() {
   const client = new MongoClient(MONGO_URI);
